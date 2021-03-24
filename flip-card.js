d3730c0672d6564716452ef9cb7ed91c1842bc3b
Awei1996 <bill010809@etech.ncyu.edu.tw>
2021-03-24 10:41:51 +0800

    Ver 6.0
    fix flip card

--- flip-card.js
+++ flip-card.js
@@ -1,35 +1,35 @@
 function flip(event){
 	var element = event.currentTarget;
 	if (element.className === "gascard") {
-    if(element.style.transform == "rotateY(180deg)") {
-      element.style.transform = "rotateY(0deg)";
+    if(element.style.webkittransform == "rotateY(180deg)") {
+      element.style.webkittransform = "rotateY(0deg)";
     }
     else {
-      element.style.transform = "rotateY(180deg)";
+      element.style.webkittransform = "rotateY(180deg)";
     }
   }
 	if (element.className === "suncard") {
-    if(element.style.transform == "rotateY(180deg)") {
-      element.style.transform = "rotateY(0deg)";
+    if(element.style.webwebkittransform == "rotateY(180deg)") {
+      element.style.webkittransform = "rotateY(0deg)";
     }
     else {
-      element.style.transform = "rotateY(180deg)";
+      element.style.webkittransform = "rotateY(180deg)";
     }
   }
 	if (element.className === "oilcard") {
-    if(element.style.transform == "rotateY(180deg)") {
-      element.style.transform = "rotateY(0deg)";
+    if(element.style.webkittransform == "rotateY(180deg)") {
+      element.style.webkittransform = "rotateY(0deg)";
     }
     else {
-      element.style.transform = "rotateY(180deg)";
+      element.style.webkittransform = "rotateY(180deg)";
     }
   }
 	if (element.className === "lifecard") {
-    if(element.style.transform == "rotateY(180deg)") {
-      element.style.transform = "rotateY(0deg)";
+    if(element.style.webkittransform == "rotateY(180deg)") {
+      element.style.webkittransform = "rotateY(0deg)";
     }
     else {
-      element.style.transform = "rotateY(180deg)";
+      element.style.webkittransform = "rotateY(180deg)";
     }
   }
 }// JavaScript Document
