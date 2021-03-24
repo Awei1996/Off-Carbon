4150fd947e302446edf40ca33db49c589a700389
Awei1996 <bill010809@etech.ncyu.edu.tw>
2021-03-24 19:06:24 +0800

    Update littleMenu.js

--- littleMenu.js
+++ littleMenu.js
@@ -13,6 +13,7 @@ var MenuValue = 0 ;
 var ShowRange = window.matchMedia("(max-width: 376px)") 
 
 
+window.matchMedia = function(){MenuShow();};
 window.onscroll = function() {MenuShow();};
 
 function MenuShow(){
